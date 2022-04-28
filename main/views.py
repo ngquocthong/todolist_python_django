98369824e2996fbe7d0d4793faf1eb8d560d1ee6
quocthongnqt <thongnguyennqt@gmail.com>
2022-04-28 10:31:35 +0700

    add models

--- main/views.py
+++ main/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ToDoList, Item
 
 # Create your views here.
-def index(response):
-    return HttpResponse("<h1>Hello! I'm Thong</h1>")
+def index(response, id):
+    ls = ToDoList.objects.get(id=id)
+    items = ls.item_set.get(id=1)
+    return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" %(ls.name, str(items.text)))
